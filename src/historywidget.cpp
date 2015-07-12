9566e358c861a0b1162872afbf14dbf79dcbdbc0
LuiCat <zhtl1995@163.com>
2015-07-12 16:53:49 +0800

    更改历史记录滚动条样式

--- src/historywidget.cpp
+++ src/historywidget.cpp
@@ -1,6 +1,8 @@
 #include "historywidget.h"
 
+#include <QApplication>
 #include <QListView>
+#include <QScrollBar>
 
 HistoryWidget::HistoryWidget(QWidget* parent)
     :QWidget(parent)
@@ -12,9 +14,15 @@ HistoryWidget::HistoryWidget(QWidget* parent)
     view->setItemDelegate(item);
     view->setModel(list);
     view->setSpacing(2);
+    view->verticalScrollBar()->setStyleSheet(
+                "QScrollBar:vertical {margin:0px 0px 0px 0px; background-color:rgb(255, 255, 255, 0); width:8px;}\
+                 QScrollBar::handle:vertical {background-color: rgb(0, 0, 0, 50); width:8px; border-radius: 4px;}\
+                 QScrollBar::sub-line:vertical {subcontrol-origin:margin; height:0px;}\
+                 QScrollBar::add-line:vertical {subcontrol-origin:margin; height:0px;}");
 
     for(int i=0;i<123;++i)
         list->append(QString("Fuck %1 times").arg(i));
+
 
 }
 
