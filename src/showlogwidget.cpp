1ef3484c52fb7c40ba4d6b683d614314e6257349
Yeehok <yeehok@gmail.com>
2015-07-13 11:19:39 +0800

    完善调试信息控件，基本功能已具备

--- src/showlogwidget.cpp
+++ src/showlogwidget.cpp
@@ -1,11 +1,31 @@
 #include "showlogwidget.h"
 
-ShowLogWidget::ShowLogWidget(QString logTitle, QWidget *parent) : QDialog(parent)
+ShowLogWidget::ShowLogWidget(const QString& logTitle, QWidget *parent) : QDialog(parent)
 {
     resize(250, 300);
     setMinimumSize(250, 300);
     setMaximumWidth(250);
 
-    mainText = new QLabel(logTitle, this);
-    mainText->setGeometry((width() - logTitle.length() * 7) / 2, 0, 250, 20);
+    titleText = new QLabel(logTitle, this);
+    titleText->setGeometry(10, 3, 250, 20);
+    mainText = new QPlainTextEdit("editddeidiede\nesavioasnvao", this);
+    mainText->setGeometry(10, 25, 230, height() - 35);
+    mainText->setReadOnly(true);
+}
+
+void ShowLogWidget::pushLine(const QString& newLogInfo)
+{
+    QString history = mainText->toPlainText() + newLogInfo + "\n";
+    mainText->setPlainText(history);
+}
+
+void ShowLogWidget::pushText(const QString & newLogInfo)
+{
+    QString history = mainText->toPlainText() + newLogInfo;
+    mainText->setPlainText(history);
+}
+
+void ShowLogWidget::paintEvent(QPaintEvent *)
+{
+    mainText->setGeometry(10, 25, 230, height() - 35);
 }
--- src/showlogwidget.h
+++ src/showlogwidget.h
@@ -4,19 +4,26 @@
 #include <QDialog>
 #include <QWidget>
 #include <QLabel>
+#include <QPlainTextEdit>
 
 class ShowLogWidget : public QDialog
 {
     Q_OBJECT
 public:
-    explicit ShowLogWidget(QString logTitle, QWidget *parent = 0);
-
+    explicit ShowLogWidget(const QString&, QWidget *parent = 0);
+    void pushLine(const QString&);
+    void pushText(const QString&);
 signals:
 
 public slots:
 
 private:
-    QLabel *mainText;
+    QLabel *titleText;
+    QPlainTextEdit *mainText;
+
+    // QWidget interface
+protected:
+    void paintEvent(QPaintEvent *);
 };
 
 #endif // SHOWLOGWIDGET_H
